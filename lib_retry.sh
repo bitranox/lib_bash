68ab9af8b822813cf56e9db583e84a0f4b48c130
bitranox <rnowotny1966@gmail.com>
2019-07-21 00:52:09 +0200

    refractor

--- lib_retry.sh
+++ lib_retry.sh
@@ -60,7 +60,7 @@ function retry_nofail {
   delay=5
   while true; do
 	command_str="${*}"
-    ${@} && break || {
+    eval "${command_str}" && break || {
       if [[ ${n} -lt ${max} ]]; then
         ((n++))
         clr_bold clr_red "Command \"${command_str}\" failed. Attempt ${n}/${max}: - no panic, we will continue after the last attempt !"
