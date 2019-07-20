a429fe0c3b0250467ca9c0025c3af1df4438b2b0
bitranox <rnowotny1966@gmail.com>
2019-07-20 22:34:50 +0200

    refractor

--- .tests/test_lib_color.sh
+++ .tests/test_lib_color.sh
@@ -9,9 +9,6 @@ function run_tests {
     # banner_base clr_green "two line ${IFS}banner_base_test"
     assert_pass "../lib_color.sh clr_cyan test-verbatim"  # call verbatim - we also get updates here
     assert_pass "clr_cyan test-sourced"
-
-
-
 }
 
 run_tests
--- .tests/test_lib_helpers.sh
+++ .tests/test_lib_helpers.sh
@@ -9,23 +9,18 @@ function run_tests {
     # banner_base clr_green "two line ${IFS}banner_base_test"
 	assert_pass "../lib_helpers.sh is_package_installed apt"  # call verbatim -  we also get updates here
 	assert_fail "../lib_helpers.sh is_package_installed unknown-package-0815"  # call verbatim
-
 	assert_pass "is_str1_in_str2 \"a\" \"aaa\""
 	assert_pass "is_str1_in_str2 \"a a\" \"aaa aaa\""
 	assert_fail "is_str1_in_str2 \"a b\" \"aaa aaa\""
-
-    assert_equal "get_linux_release_number" "19.04"
-    assert_equal "get_linux_release_number_major" "19"
+    assert_pass "get_linux_release_number"
+    assert_pass "get_linux_release_number_major"
 	assert_pass "is_package_installed apt"
 	assert_fail "is_package_installed unknown-package-0815"
     # assert_equal "get_prepend_auto_configuration_message_to_line test" "auto configured by bitranox configmagick scripts at 2019-07-17 12:53:05\\ntest"
     assert_fail "is_script_sourced"
     assert_fail "is_hetzner_virtual_server"
     assert_equal "get_home_directory_from_username ${USER}" "/home/${USER}"
-
-
     assert_equal "echo \"printenv USER: $(printenv USER)\" : $USER" "printenv USER: $USER : $USER"  # check if env user is the same as the Variable $USER
-
 }
 
 run_tests
