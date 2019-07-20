8e75a615c6cef44ce26f23ae8eae20e41b00e2f3
bitranox <rnowotny1966@gmail.com>
2019-07-20 22:38:40 +0200

    refractor

--- .tests/test_lib_color.sh
+++ .tests/test_lib_color.sh
@@ -7,7 +7,7 @@ function run_tests {
 	# dummy_test 2>/dev/null || clr_green "no tests in ${BASH_SOURCE[0]}"
     # banner_base clr_green "one line banner_base_test"
     # banner_base clr_green "two line ${IFS}banner_base_test"
-    assert_pass "../lib_color.sh clr_cyan test-verbatim"  # call verbatim - we also get updates here
+    assert_pass "../lib_color.sh clr_blue test-verbatim"  # call verbatim - we also get updates here
     assert_pass "clr_cyan test-sourced"
 }
 
