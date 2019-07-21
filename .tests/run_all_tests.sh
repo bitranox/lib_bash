be7eb9a4d6636f52e7eef94c5f010ae4f4b739a8
bitranox <rnowotny1966@gmail.com>
2019-07-21 23:22:24 +0200

    refractor

---
+++ .tests/run_all_tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source ../lib_helpers.sh
+
+sudo_askpass="$(command -v ssh-askpass)"
+export SUDO_ASKPASS="${sudo_askpass}"
+export NO_AT_BRIDGE=1  # get rid of (ssh-askpass:25930): dbind-WARNING **: 18:46:12.019: Couldn't register with accessibility bus: Did not receive a reply.
+
+function run_all_tests {
+    # shellcheck disable=SC2164
+    # local my_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"  # this gives the full path, even for sourced scripts
+    local files_to_test actual_file_to_test
+
+    mapfile -t files_to_test < <(ls ./test_*.sh)
+    for actual_file_to_test in "${files_to_test[@]}"
+    do
+        "${actual_file_to_test}"
+    done
+    ./shellcheck.sh
+    clr_green "test ok in $(get_own_script_name "${BASH_SOURCE[0]}")"
+    sleep 1
+}
+
+if [[ "${0}" == "${BASH_SOURCE}" ]]; then    # if the script is not sourced
+    run_all_tests
+fi
