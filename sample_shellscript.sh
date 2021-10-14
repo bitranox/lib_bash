560aa8542aacb9aea9ec71d423ac5e4b4656dd7e
bitranox <rnowotny1966@gmail.com>
2021-10-14 21:26:03 +0200

    refractor

--- sample_shellscript.sh
+++ sample_shellscript.sh
@@ -16,6 +16,7 @@ function include_dependencies {
     source /usr/local/lib_bash/install_or_update.sh
     if ! is_lib_bash_up_to_date; then
       update_lib_bash
+      # shellcheck disable=SC1090
       source "$(readlink -f "${BASH_SOURCE[0]}")"      # source ourself
       exit 0                                           # exit the old instance
     fi
@@ -25,8 +26,8 @@ include_dependencies
 
 clr_green "Hello World"
 clr_red   "Hello World"
-clr_blue   "Hello World"
-clr_yellow "Hello World"
+clr_blue  "Hello World"
+clr_cyan  "Hello World"
 
 read -rp "Finished, press any key to continue... " -n1 -s
 echo ""
