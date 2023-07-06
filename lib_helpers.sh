71abc1a26556feb64b6f9c67aee03b610e3f03a1
bitranox <rnowotny1966@gmail.com>
2023-07-06 17:20:50 +0200

    introduce PEP517 installer

--- lib_helpers.sh
+++ lib_helpers.sh
@@ -658,6 +658,5 @@ function lib_bash_get_hostname_short {
     echo "${hostname_short}"
 }
 
-
 ## make it possible to call functions without source include
 call_function_from_commandline "${0}" "${@}"
