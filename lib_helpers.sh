7aea1897fe67bae161ec888011057a50f5290009
bitranox <rnowotny1966@gmail.com>
2025-01-24 12:56:11 +0100

    self_update refractor

--- lib_helpers.sh
+++ lib_helpers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Dieses Skript dient als "Launcher" für lib_bash
+echo "DEPRECATED - just import /usr/local/lib_bash/lib_bash.sh"
 
 # Holen des absoluten Pfads des Skripts selbst (funktioniert bei Sourcing und Execution)
 declare -r LIB_BASH_SELF="$(readlink -f "${BASH_SOURCE[0]}")"
