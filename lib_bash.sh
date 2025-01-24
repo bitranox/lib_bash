604422320b5e1c53c7c4db1e8f23e13862968119
bitranox <rnowotny1966@gmail.com>
2025-01-24 19:57:04 +0100

    self_update refractor

--- lib_bash.sh
+++ lib_bash.sh
@@ -1036,7 +1036,7 @@ function LIB_BASH_MAIN {
 
 # update myself in a subshell - only once per session
 if [[ ! -v LIB_BASH_IS_UP_TO_DATE ]]; then
-    LIB_BASH_IS_UP_TO_DATE="true" &>/dev/null
+    declare -r LIB_BASH_IS_UP_TO_DATE="true" &>/dev/null
     (
     # shellcheck disable=SC2034
     LIB_BASH_SELF_UPDATE_SELF=$(readlink -f "${BASH_SOURCE[0]}")
--- self_update.sh
+++ self_update.sh
@@ -195,7 +195,7 @@ function lib_bash_self_update {
 }
 
 
-if ! declare -F "source_lib_bash_dependencies" >/dev/null 2>&1
-then
+if ! declare -F "source_lib_bash_dependencies" &>/dev/null; then
+    declare -r LIB_BASH_IS_UP_TO_DATE="True"  &>/dev/null
     source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/lib_bash.sh"
 fi
