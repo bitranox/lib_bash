226f854568f493f08beb05d24247692749d901c1
bitranox <rnowotny1966@gmail.com>
2025-01-24 12:42:12 +0100

    self_update refractor

--- sample_self_update_main_script.sh
+++ sample_self_update_main_script.sh
@@ -10,7 +10,9 @@ function MAIN {
 
 # update myself in a subshell
 (
+# shellcheck disable=SC2034
 LIB_BASH_SELF_UPDATE_SELF=$(readlink -f "${BASH_SOURCE[0]}")
+# shellcheck disable=SC2034
 LIB_BASH_SELF_UPDATE_SELF_MAIN_FUNCTION="MAIN"
 source /usr/local/lib_bash/self_update.sh
 lib_bash_self_update "$@"
