56e415e397a5a8ded2c86b46eed05737a34d626a
bitranox <rnowotny1966@gmail.com>
2025-01-25 02:19:05 +0100

    change lib_bash

--- lib_bash.sh
+++ lib_bash.sh
@@ -1064,8 +1064,9 @@ function _lib_bash_self_update {
     # if ! _user_is_allowed_to_update; then return 0; fi
     git config --global --add safe.directory /usr/local/lib_bash
     local current_hash=$(git -C "$script_dir" rev-parse HEAD )
-    local current_hash=$(git -C "$script_dir" rev-parse HEAD 2>/dev/null)
-    local remote_hash=$(git -C "$script_dir" ls-remote origin HEAD 2>/dev/null | awk '{print $1}')
+    # local current_hash=$(git -C "$script_dir" rev-parse HEAD 2>/dev/null)
+    local remote_hash=$(git -C "$script_dir" ls-remote origin HEAD | awk '{print $1}')
+    # local remote_hash=$(git -C "$script_dir" ls-remote origin HEAD 2>/dev/null | awk '{print $1}')
     if [[ "$remote_hash" != "$current_hash" ]] && [[ -n "$remote_hash" ]]; then
         log "New version available, updating..."
         git -C "$script_dir" fetch --all
